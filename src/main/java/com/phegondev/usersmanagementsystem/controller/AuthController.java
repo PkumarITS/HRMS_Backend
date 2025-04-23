ac2e456cb326c0f64afa1d521be6dacbb2ba9541
PkumarITS <pkumar1@infinevocloud.com>
2025-04-23 10:36:07 +0530

    all backend integrated

--- src/main/java/com/phegondev/usersmanagementsystem/controller/AuthController.java
+++ src/main/java/com/phegondev/usersmanagementsystem/controller/AuthController.java
@@ -16,7 +16,7 @@ import java.util.Set;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
